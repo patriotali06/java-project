05f4fa27f71b9e797d167ceaab0c6652d5533d3d
ali <patriotali06@gmail.com>
2020-06-17 23:37:16 -0400

    first commit

---
+++ src/main/java/com/ali/App.java
@@ -0,0 +1,13 @@
+package com.ali;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
